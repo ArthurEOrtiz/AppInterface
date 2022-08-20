69250c0008ce28cb0fcb1155d3ccc8740080e4ad
ArthurEOrtiz <ortiz.arthur.e@gmail.com>
2022-08-20 11:25:38 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
